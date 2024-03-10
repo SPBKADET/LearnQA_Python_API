45e11e5390920ab0709f74f69fedcce4932c17f1
Spbkadet <spbkadet@mail.ru>
2024-03-10 12:40:24 +0300

    16 exersice

---
+++ tests/test_user_get.py
@@ -0,0 +1,19 @@
+import requests
+
+from lib.base_case import BaseCase
+
+
+class TestUserGet(BaseCase):
+    def test_user_get_with_invalid_id(self):
+        email = 'vinkotovexample.com'
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
+        response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
+        response1 = requests.get('https://playground.learnqa.ru/api/user/2')
+        assert response1.status_code == 200
+        assert response1.json()['username'] == 'Vitaliy'
